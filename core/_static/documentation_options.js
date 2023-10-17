0c9cfd22d24201ff0aeca3635777fa6b75fc9bf5
littledan <littledan@users.noreply.github.com>
2023-10-17 16:18:48 +0000

    deploy: 26e6faa9762b604e8eea399be1e8a1c3bda256ab

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 (Draft 2023-07-06)',
+    VERSION: '1.1 (Draft 2023-10-17)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
