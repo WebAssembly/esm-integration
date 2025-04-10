840fa5b430ca0b555bdcf3cef332686987ae8a63
guybedford <guybedford@users.noreply.github.com>
2025-04-10 16:39:42 +0000

    deploy: 03354b6e85c731ace0b515456448d01badd8943d

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2025-02-11)',
+    VERSION: '2.0 (Draft 2025-04-10)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
